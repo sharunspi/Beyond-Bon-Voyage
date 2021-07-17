b65cb888358e5103956b8c61d5ea3cf6480ca661
sharunspi <sharunkk1997@gmail.com>
2021-07-17 21:31:17 +0530

    added status bar

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -3,10 +3,18 @@ import From from '../components/forms/From'
 export default function MainWindow() {
     const [currentStage,setCurrentStage] = useState(1)
     return (
-        <div className='main-card'>
+  <div className="main-window">
+            <div className='main-card'>
             <div className='container'>
                      <From/>
+         
             </div>
+            <div className='status'>
+        
         </div>
+         
+        </div>
+
+  </div>
     )
 }
