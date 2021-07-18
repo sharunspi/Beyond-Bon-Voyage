077dcff20487c642c538c3109528253d495b59ad
sharunspi <sharunkk1997@gmail.com>
2021-07-18 10:37:14 +0530

    added vehicles

--- src/components/forms/Vehicle.js
+++ src/components/forms/Vehicle.js
@@ -1,12 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {vehicles} from '../../data.json'
+import rocket from '../../assets/images/rocket.png'
 export default function Vehicle(props) {
+    const [selectedVehicle,setSelectedVehicle] = useState('')
     const stepChange = stp =>{
         props.nextStep(stp)
     }
+    const newVehicle =(newV)=>{
+        setSelectedVehicle(newV)
+    }
+    const Card =(props)=>{
+        return <div  onClick={()=>newVehicle(props.name)}>
+            <img className={selectedVehicle == props.name ? "vehicle vehicle-selected":"vehicle"} src={rocket}/>
+            <div>
+                {props.name}
+            </div>
+        </div>
+    }
     return (
         <div>
-            vehicle
+            <div>
+                {
+                    vehicles.map(vehi=>{
+                        return <Card name={vehi.name} />
+                    })
+                }
+            </div>
                  <div className="footer">
                     <button onClick={()=>stepChange(props.id)} >
                         Next
