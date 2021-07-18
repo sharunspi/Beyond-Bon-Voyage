1fd6ea5a99abded2cf3b3037d46df261554ad14b
sharunspi <sharunkk1997@gmail.com>
2021-07-18 10:53:50 +0530

    added sub files

--- src/components/forms/To.js
+++ src/components/forms/To.js
@@ -1,12 +1,30 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {expolanents} from '../../data.json'
+import planetImage from '../../assets/images/planet.png' 
 export default function To(props) {
+    const [planetSelected,setPlanetSelected] = useState('')
     const stepChange = stp =>{
         props.nextStep(stp)
     }
+    const newPlanet = pln=>{
+        setPlanetSelected(pln)
+    }
+    const Planet =(props)=>{
+        return <>
+            <div onClick={()=>newPlanet(props.name)}>
+                <img className={planetSelected == props.name ? "planet select-planet":"planet"} src={planetImage}/>
+            </div>
+        </>
+    }
     return (
         <div>
-            to
+            <div>
+                {
+                    expolanents.map(planet=>{
+                        return <Planet name={planet.name}/>
+                    })
+                }
+            </div>
                  <div className="footer">
                     <button onClick={()=>stepChange(props.id)} >
                         Next
